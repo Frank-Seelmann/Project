31cad7934e62e0f243fbb685c6a8de8825a6a680
frankdaseel <frankseelmann123@gmail.com>
2022-04-21 14:27:50 -0400

    added CORS and user functions

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const userRoutes = require('./server/routes/user');
+
+app.use(express.json()); //To parse JSON bodies (Applicable for Express 4.16+)
+
+app.use(express.static(__dirname + "/public"));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/home.html')))
+
+//CORS middleware
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");  
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");  
+  next();
+});
 
 app.use("/users", userRoutes);
 
--- server/models/user.js
+++ server/models/user.js
@@ -22,12 +22,35 @@ let getUsers = () => users;
 
 async function login(username, password) {
     const user = users.filter((u) => u.userName === username);
-    if(!user) throw Error("User not found");
-    if(user[0].userPassword !== password) throw Error("Wrong password");
+    if (!user) throw Error("User not found");
+    if (user[0].userPassword !== password) throw Error("Wrong password");
 
     this.currentUser = user[0];
     return user[0];
 };
 
+function register(user) {
+    const u = userExists(user.username);
+    if (u.length > 0) throw Error('Username in use!');
+
+    const newUser = {
+        userId: users[users.length - 1].userId + 1,
+        userName: user.username,
+        password: user.password
+    };
+    users.push(newUser);
+    return newUser;
+}
+
+function deleteUser(userId) {
+    let i = users.map((user) => user.userId).indexOf(userId);
+    users.splice(i, 1);
+    console.log(users);
+}
+
+function userExists(username) {
+    return users.filter((u) => u.userName === username);
+}
+
 //export to use in other files
-module.exports = { getUsers, login };
+module.exports = { getUsers, login, register, deleteUser };
