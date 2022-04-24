c25552dbb44edd7fd483ca8eb36e461cf3807f87
frankdaseel <frankseelmann123@gmail.com>
2022-04-24 11:33:00 -0400

    small fixes

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 
 const userRoutes = require('./server/routes/user');
+const projectRoutes = require('./server/routes/project');
+const commentRoutes = require('./server/routes/comment');
+const profileRoutes = require('./server/routes/profile');
 
 app.use(express.json()); //To parse JSON bodies (Applicable for Express 4.16+)
 
@@ -18,6 +21,9 @@ app.use(function(req, res, next) {
 });
 
 app.use("/users", userRoutes);
+app.use("/projects", projectRoutes);
+app.use("/comments", commentRoutes);
+app.use("/profiles", profileRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
